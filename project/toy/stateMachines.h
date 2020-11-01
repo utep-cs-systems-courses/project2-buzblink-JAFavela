0898734b4f55a466a90b589e31320527a7400d51
JAFavela <jafavela@miners.utep.edu>
2020-11-01 00:47:11 -0600

    added a song to my working program and broke it for now

--- project/toy/stateMachines.h
+++ project/toy/stateMachines.h
@@ -2,5 +2,12 @@
 #define stateMachine_included
 
 void state_advance();
+void siren();
+void dim_rg_led();
+void led_switch(int note);
+void alternate_led();
+void chona();
+
 static char state = 0;
+
 #endif // included
